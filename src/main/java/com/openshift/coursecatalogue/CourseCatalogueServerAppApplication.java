e49295e6cbb2134d11a833c38aaa029a985048ca
saiprasadam <32019169+saiprasadam@users.noreply.github.com>
2020-08-13 10:00:40 +0530

    Update CourseCatalogueServerAppApplication.java

--- src/main/java/com/openshift/coursecatalogue/CourseCatalogueServerAppApplication.java
+++ src/main/java/com/openshift/coursecatalogue/CourseCatalogueServerAppApplication.java
@@ -2,9 +2,20 @@ package com.openshift.coursecatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class CourseCatalogueServerAppApplication {
+public class CourseCatalogueServerAppApplication extends SpringBootServletInitializer {
+
+    @Override
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(SpringBootWarDeploymentApplication.class);
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseCatalogueServerAppApplication.class, args);
